cb8d505ff629a54c3acbdb5dbe650ee275dbcd25
OskarPaul93 <paul_1993oscar@hotmail.com>
2026-08-14 00:46:43 -0500

    Metodo: Recargar cerveza

--- src/com/krakedev/artesanal/Maquina.java
+++ src/com/krakedev/artesanal/Maquina.java
@@ -28,6 +28,18 @@ public class Maquina {
 	
 	public void llenarMaquina() {
 		this.cantidadActual= this.capacidadMaxima -100;
+	}
+	
+	public boolean recargarCerveza(double cantidad) {
+		double limitePermitido;
+		limitePermitido= capacidadMaxima - 100;
+		
+		if(cantidadActual + cantidad <= limitePermitido) {
+			cantidadActual = cantidadActual + cantidad;
+			return true;
+		}else {
+			return false;
+		}
 		
 	}
 	
