cf793306d3e6f4f71cd7805050f94f930f7ecb96
OskarPaul93 <paul_1993oscar@hotmail.com>
2026-08-14 00:20:24 -0500

    Primer metodo: LLenar maquina

---
+++ src/com/krakedev/artesanal/test/TestAtributos.java
@@ -0,0 +1,18 @@
+package com.krakedev.artesanal.test;
+
+import com.krakedev.artesanal.Maquina;
+
+public class TestAtributos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Maquina rubia = new Maquina("Pilsener", "cerveza rubia", 0.02, 20000);
+		rubia.imprimir();
+		
+		//El set cambia el valor del atributo
+		rubia.setNombreCerveza("Golden");
+		rubia.setDescripcion("Cerveza con aroma mas intenso");
+		rubia.imprimir();
+	}
+
+}
