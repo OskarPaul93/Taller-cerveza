5beed62c83b213b65e26e4464c22bb17ef75c39c
OskarPaul93 <paul_1993oscar@hotmail.com>
2026-09-24 22:44:09 -0500

    Se corrige el error de NullPointerException

--- src/com/krakedev/artesanal/NegocioMejorado.java
+++ src/com/krakedev/artesanal/NegocioMejorado.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class NegocioMejorado {
 	
 	public ArrayList<Maquina> maquinas;
-	private ArrayList<Cliente> clientes;
+	private ArrayList<Cliente> clientes = new ArrayList<>();
 	private int ultimoCodigo =100;
 	
 	public void registrarCliente (String nombre, String cedula) {
